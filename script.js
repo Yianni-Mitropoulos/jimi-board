689cb4e620aa2d223fd365a9b671355435074e86
Yianni Mitropoulos <yianni.mitropoulos@gmail.com>
2024-06-12 01:49:54 +1000

    Slider seems to work

--- script.js
+++ script.js
@@ -5,26 +5,33 @@ const three          = Math.pow(2, 19/12) // A number that approximately equals 
 const half_comma     = 0.5 * 12 * Math.log2(three*three*three*three/80) * 0.93;
 
 // White keys
-const semitones      = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
-const ji_ratios      = [1/1, 17/16, three*three/8, 19/16, 5/4, 4/three, 17/12, three/2, Math.sqrt((30/19)*(19/12)), 5/three, three*10/17, 15/8];
-const ji_semitones   = ji_ratios.map((ratio) => 12*Math.log2(ratio));
-const ji_corrections = ji_semitones.map((semitone, index) => semitone - index + half_comma);
+const ji_ratios      = [[1/1], [17/16], [three*three/8], [19/16], [5/4], [4/three], [17/12], [three/2], [30/19, 19/12], [5/three], [three*10/17], [15/8]];
+const ji_semitones   = ji_ratios.map((ratiolist) => ratiolist.map((ratio) => 12*Math.log2(ratio)));
+const ji_corrections = ji_semitones.map((semitonelist, index) => semitonelist.map((semitone) => semitone - index + half_comma));
 
 // Grey keys
-const microtonal_semitones = [0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5,10.5,11.5,12.5,13.5,14.5,15.5,16.5,17.5,18.5,19.5,20.5,21.5,22.5,23.5];
-const microtonal_ji_ratios = [NaN, 13/(4*three), 7/(2*three), NaN, NaN, 11/8, NaN, NaN, 13/8, 7/4, 11/(three*2), NaN];
-const microtonal_ji_semitones = microtonal_ji_ratios.map((ratio) => 12*Math.log2(ratio));
-const microtonal_ji_corrections = microtonal_ji_semitones.map((semitone, index) => {
-    if (isNaN(semitone)) {
-        return half_comma;
+const microtonal_ji_ratios = [[], [5*three/14, 13/(4*three)], [5*three/13, 7/(2*three)], [], [], [5*three/11, 11/8], [], [], [13/8], [7/4], [11/(three*2)], []];
+const microtonal_ji_semitones = microtonal_ji_ratios.map((ratiolist) => ratiolist.map((ratio) => 12*Math.log2(ratio)));
+const microtonal_ji_corrections = microtonal_ji_semitones.map((semitonelist, index) => {
+    if (semitonelist.length == 0) {
+        return [half_comma];
     } else {
-        return semitone - (index + 0.5) + half_comma;
+        return semitonelist.map((semitone) => semitone - (index + 0.5) + half_comma);
     }
 });
 
+avg = (L) => {
+    if (L.length == 2) {
+        let r = parseInt(slider.value, 10)/parseInt(slider.max, 10);
+        return (1-r)*L[0] + r*L[1];
+    } else {
+        return L[0];
+    }
+}
+
 var mod = (n, m) => {
-    var remain = n % m;
-    return Math.floor(remain >= 0 ? remain : remain + m);
+    var remainder = n % m;
+    return Math.floor(remainder >= 0 ? remainder : remainder + m);
 };
 
 syntheticSound = (semitone) => {
@@ -65,17 +72,30 @@ syntheticSound = (semitone) => {
 }
 
 function playSound(index) {
-    console.log(index, semitones[index]);
-    syntheticSound(semitones[index]);
+    let semitone = index + scale_parity * avg(ji_corrections[modular_relative_offset(index)]);
+    console.log(index, semitone);
+    syntheticSound(semitone);
 }
 
 function playMicrotonalSound(index) {
-    console.log(index, microtonal_semitones[index]);
-    syntheticSound(microtonal_semitones[index]);
+    if (scale_parity === 0) {
+        semitone = index + 0.5;
+    } else {
+        if (scale_parity === 1) {
+            mro = modular_relative_offset(index);
+        } else {
+            mro = modular_relative_offset(index+1);
+        }
+        console.log(mro);
+        semitone = index + 0.5 + scale_parity * avg(microtonal_ji_corrections[mro]);
+    }
+    console.log(index, semitone);
+    syntheticSound(semitone);
 }
 
 whiteboard    = document.getElementById("whiteboard");
 greyboard     = document.getElementById("greyboard");
+slider        = document.getElementById("slider"); 
 circle        = document.getElementById("circle");
 circle_button = undefined
 
@@ -99,6 +119,10 @@ set_jimiboard_greykey_class = (i, modular_relative_offset) => {
     }    
 }
 
+offset = 0;
+modular_relative_offset = (j) => 0;
+scale_parity = 0;
+
 function setScale(index) {
     if (circle_button) {
         circle_button.classList.toggle('btn-emphasized');
@@ -106,33 +130,46 @@ function setScale(index) {
     circle_button = circle.children[index];
     circle_button.classList.toggle('btn-emphasized');
     if (index % 2 == 0) {
+        scale_parity = 1;
         offset = (7*Math.round(index/2)) % 12;
+        modular_relative_offset = (j) => mod(j - offset, 12);
         // Major chromatic scale
-        for (let i=0; i<semitones.length; i++) {
-            let modular_relative_offset = mod(i - offset, 12);
-            semitones[i] = i + ji_corrections[modular_relative_offset];
-            set_jimiboard_whitekey_class(i, modular_relative_offset);
+        for (let i=0; i<whiteboard.children.length; i++) {
+            set_jimiboard_whitekey_class(i, modular_relative_offset(i));
         }
         // Major microtonal notes
-        for (let i=0; i<microtonal_semitones.length-1; i++) {
-            let modular_relative_offset = mod(i - offset, 12);
-            microtonal_semitones[i] = i + 0.5 + microtonal_ji_corrections[modular_relative_offset];
-            set_jimiboard_greykey_class(i, modular_relative_offset);
+        for (let i=0; i<greyboard.children.length; i++) {
+            set_jimiboard_greykey_class(i, modular_relative_offset(i));
         }
     } else {
+        scale_parity = -1;
         index -= 1;
         offset = (11 + (7*Math.round(index/2))) % 12;
+        modular_relative_offset = (j) => mod(offset - j, 12);
         // Minor chromatic scale
-        for (let i=0; i<semitones.length; i++) {
-            let modular_relative_offset = mod(offset - i, 12);
-            semitones[i] = i - ji_corrections[modular_relative_offset];
-            set_jimiboard_whitekey_class(i, modular_relative_offset);
+        for (let i=0; i<whiteboard.children.length; i++) {
+            set_jimiboard_whitekey_class(i, modular_relative_offset(i));
         }
         // Minor microtonal notes
-        for (let i=0; i<microtonal_semitones.length-1; i++) {
-            let modular_relative_offset = mod(offset - i, 12) - 1;
-            microtonal_semitones[i] = i + 0.5 - microtonal_ji_corrections[modular_relative_offset];
-            set_jimiboard_greykey_class(i, modular_relative_offset);
+        for (let i=0; i<greyboard.children.length; i++) {
+            set_jimiboard_greykey_class(i, modular_relative_offset(i) - 1);
         }
     }
+}
+
+reset = () => {
+    offset = 0;
+    modular_relative_offset = (j) => 0;
+    scale_parity = 0;
+    if (circle_button) {
+        circle_button.classList.toggle('btn-emphasized');
+        circle_button = undefined;
+    }
+    for (let i=0; i<whiteboard.children.length; i++) {
+        whiteboard.children[i].classList.remove('btn-emphasized')
+    }
+    for (let i=0; i<greyboard.children.length; i++) {
+        greyboard.children[i].classList.remove('btn-emphasized')
+    }
+    slider.value = slider.max;
 }
